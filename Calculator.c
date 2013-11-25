537c42d043eaf1cb004a944b9b7c02fecc4a3ab3
Ratih CMA <Ratih@Ratihface.(none)>
2013-11-25 10:21:42 +0900

    commit

---
+++ Calculator.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void PrintError()
+{
+	printf("\n!!!! エラー：計算式が正しくありません。!!!!\n");
+}
+
+int CalcValue( int a, int b, char symbol )
+{	
+	int returnValue = a;
+	switch( symbol ) {
+		case '+':
+			returnValue += b;
+			break;
+		case '-':
+			returnValue -= b;
+			break;
+		case '0':
+			returnValue = b;
+			break;
+	}
+	
+	return returnValue;
+} 
+
+
+int main( int argc, char* argv[] )
+{
+	printf("入力された計算式：");
+	int i;
+	for(i = 0; i < argc; i++) {
+		printf("%s ", argv[i]);
+	}
+	printf("\n");	
+	printf("引数の数：%d\n", argc);
+	
+	int result = 0;
+	char symbol = '+';
+	
+	for(i = 1; i < argc; i++) {
+		int numCheck;		
+		switch( *argv[i] ) {
+			case '+':
+			case '-':
+				// 既に符号データが入力されていた場合
+				if ( symbol != 0 ) {
+					PrintError();
+					return 0;
+				} 
+			
+				 symbol = *argv[i];
+				 break;
+			default:
+				 numCheck = atoi( argv[i] ); 
+				 if ( numCheck == 0 ) {
+					PrintError();
+					return 0;
+				 }
+				 
+				 result = CalcValue( result, numCheck, symbol );
+				 symbol = 0;
+		}
+	}
+	
+	printf("計算結果：%d", result);
+	
+	return 0;
+}
+
+
